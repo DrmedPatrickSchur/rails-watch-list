a837ec3dbbfa6fea48c2d50907443810abb63b5c
Patrick Schur <patrick.schur@icloud.com>
2023-02-23 16:24:30 +0100

    for now

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,33 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @list = List.create(list_params)
+    if @list.save
+      redirect_to lists_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_lsit
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
 end
