bd29267d035a3383794e1e1feaf01ec66efb555e
Patrick Schur <patrick.schur@icloud.com>
2023-02-23 14:59:22 +0100

    Prepare rails app with external specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Defines the root path route ("/")
+# root "articles#index"
+  get    "lists",          to: "lists#index",  as: :lists
+  get    "lists/new",      to: "lists#new",    as: :new_list
+  post   "lists",          to: "lists#create"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get    "lists/:id",      to: "lists#show",   as: :list
+
+  get    "lists/:id/edit", to: "lists#edit",   as: :edit_list
+  patch  "lists/:id",      to: "lists#update"
+
+  delete "lists/:id",      to: "lists#destroy"
 end
